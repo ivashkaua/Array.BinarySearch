aef13a74dc07adb5024fc043f420e77039463d6f
ivashkaua <86633081+ivashkaua@users.noreply.github.com>
2021-09-28 02:45:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Array.BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: \t");
+            int ArrNumber = int.Parse(Console.ReadLine());
+            int[] array = new int[ArrNumber];
+
+            for (int l = 0; l < ArrNumber; l++)
+            {
+                Console.Write($"\nВведите число под номером {l + 1} \t");
+                array[l] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.Write("Введите число для определения индекса: \t");
+            int value = int.Parse(Console.ReadLine());
+
+
+            for (int z = 0; z < array.Length; z++)
+            {
+                for (int m = 0; m < array.Length - 1; m++)
+                {
+                    if (array[m] > array[m + 1])
+                    {
+                        int temp = array[m + 1];
+                        array[m + 1] = array[m];
+                        array[m] = temp;
+                    }
+                }
+            }
+
+            int L = 0;
+            int R = ArrNumber;
+            int k;
+
+            while (L < R)
+            {
+                k = (R + L) / 2;
+                if (array[k] == value)
+                    Console.WriteLine("\nИндекс числа в массиве: " + "\t" + k);
+                if (array[k] < value)
+                    L = k;
+                else
+                    R = k;
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+
+}
